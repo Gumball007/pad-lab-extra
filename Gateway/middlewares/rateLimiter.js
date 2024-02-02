0d11689e9ece7a4e007367e90d072f3142994e07
Ana Coroletchi <anacoroletchi@yahoo.com>
2024-02-03 01:48:11 +0200

    add env variables for rate limiter

--- Gateway/middlewares/rateLimiter.js
+++ Gateway/middlewares/rateLimiter.js
@@ -1,9 +1,12 @@
 import rateLimit from 'express-rate-limit';
 
+const TIME_WINDOW = process.env.TIME_WINDOW || 60;
+const MAX_TASKS = process.env.MAX_TASKS || 50;
+
 export const rateLimiterUsingThirdParty = rateLimit({
   windowMs: 60 * 1000,
   max: 100,
-  message: 'You have exceeded the 3 requests in 1 minute limit!', 
+  message: 'You have exceeded requests limit!', 
   standardHeaders: true,
   legacyHeaders: false,
 });
